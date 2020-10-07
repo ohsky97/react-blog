7ba0bb220c6cf5d1691f7c037983f01887912969
ohsky97 <ohsky97@gmail.com>
2020-10-07 16:16:07 +0900

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Navigation user = {user} />
         <Switch>
           <Route path="/:page" component={PageRenderer} />
-          <Route path="/" render = {() => <Redirect to="/react-blog/home" />} />
+          <Route path="/" render = {() => <Redirect to="/react-blog" />} />
           <Route component={() => 404} />
         </Switch>
       </div>
